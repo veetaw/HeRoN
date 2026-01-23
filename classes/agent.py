db06e9b629a15a7461c3ffb25bf6d1f66b45985a
Vito <13486742+veetaw@users.noreply.github.com>
2026-01-23 15:46:05 +0100

    fix epsilon decay

--- classes/agent.py
+++ classes/agent.py
@@ -102,9 +102,8 @@ class DQNAgent:
             target_f[0][action] = target
             self.model.fit(state, target_f, epochs=1, verbose=0)
         
-        # RIMOSSO: epsilon decay ora avviene per episodio, non per batch
-        # if self.epsilon > self.epsilon_min:
-        #     self.epsilon *= self.epsilon_decay
+        if self.epsilon > self.epsilon_min:
+             self.epsilon *= self.epsilon_decay
 
     def save(self, path_prefix):
         self.model.save(f"{path_prefix}.keras") # enter model path
